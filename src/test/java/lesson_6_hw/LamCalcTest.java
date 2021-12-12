4be5ac0992eb7d9c7ef50f999666044d4a0da845
QA-Alexandr-Lisovskij <lisovskijsanya0@mail.ru>
2021-12-12 19:28:15 +0300

    My Homework

--- src/test/java/lesson_6_hw/LamCalcTest.java
+++ src/test/java/lesson_6_hw/LamCalcTest.java
@@ -2,9 +2,11 @@ package lesson_6_hw;
 
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -15,6 +17,15 @@ public class LamCalcTest {
     public ChromeDriver driver;
 
 
+    public void inputValue(String keys,WebElement element){
+        if(keys.isEmpty()){
+            return;
+        }else{
+            element.sendKeys(Keys.CONTROL + "a");
+            element.sendKeys(Keys.DELETE);
+            element.sendKeys(keys);
+        }
+    }
 
     @BeforeTest
     public void setUp(){
@@ -28,14 +39,14 @@ public class LamCalcTest {
     @Test
     public void checkEUHCalc() {
         //Описание переменных
-        String roomWidth = "6";
-        String roomHeight = "3";
-        String lamWidth = "1300";
-        String lamHeight = "192";
-        String inPack = "12";
-        String price = "500";
-        String bias = "300";
-        String wallDist = "10";
+        String roomWidthValue = "6";
+        String roomHeightValue = "3";
+        String lamWidthValue = "1300";
+        String lamHeightValue = "192";
+        String inPackValue = "12";
+        String priceValue = "500";
+        String biasValue = "300";
+        String wallDistValue = "10";
         String expectedSLam = "17.82 м2.";
         String expectedCount = "76 шт.";
         String expectedPacks = "7 шт.";
@@ -47,26 +58,47 @@ public class LamCalcTest {
         driver.get("https://masterskayapola.ru/kalkulyator/laminata.html");
 
         //Описание элементов страницы
-        WebElement weight = driver.findElement(By.id("el_f_width"));
-        WebElement length = driver.findElement(By.id("el_f_lenght"));
-        WebElement roomElement = driver.findElement(By.id("room_type"));
-        Select typeRoom = new Select(roomElement);
-        WebElement heatingElement = driver.findElement(By.id("heating_type"));
-        Select typHeating = new Select(heatingElement);
-        WebElement heatLoss = driver.findElement(By.id("el_f_losses"));
-        WebElement calculate = driver.findElement(By.cssSelector("input[type = 'button']"));
+        WebElement roomWidth = driver.findElement(By.name("calc_roomwidth"));
+        WebElement roomHeight = driver.findElement(By.name("calc_roomheight"));
+        WebElement lamWidth = driver.findElement(By.name("calc_lamwidth"));
+        WebElement lamHeight = driver.findElement(By.name("calc_lamheight"));
+        WebElement inPack = driver.findElement(By.name("calc_inpack"));
+        WebElement price = driver.findElement(By.name("calc_price"));
+        WebElement bias = driver.findElement(By.name("calc_bias"));
+        WebElement wallDist = driver.findElement(By.name("calc_walldist"));
+        WebElement calc_direct = driver.findElement(By.name("calc_direct"));
+        Select direction = new Select(calc_direct);
+        WebElement calculate = driver.findElement(By.cssSelector("input[value='Рассчитать']"));
 
         //Выполнение теста
 
-        typeRoom.selectByIndex(1);
-        typHeating.selectByIndex(1);
+        inputValue(roomWidthValue,roomWidth);
+        inputValue(roomHeightValue,roomHeight);
+        inputValue(lamWidthValue,lamWidth);
+        inputValue(lamHeightValue,lamHeight);
+        inputValue(inPackValue,inPack);
+        inputValue(priceValue,price);
+        inputValue(biasValue,bias);
+        inputValue(wallDistValue,wallDist);
 
+        direction.selectByIndex(0);
         calculate.click();
-        String actualFloorCablePower = driver.findElement(By.id("floor_cable_power")).getAttribute("value");
-        String actualSpecFloorCablePower = driver.findElement(By.id("spec_floor_cable_power")).getAttribute("value");
 
+        String actualSLam = driver.findElement(By.id("s_lam")).getText();
+        String actualCount = driver.findElement(By.id("l_count")).getText();
+        String actualPacks = driver.findElement(By.id("l_packs")).getText();
+        String actualPrice = driver.findElement(By.id("l_price")).getText();
+        String actualOver = driver.findElement(By.id("l_over")).getText();
+        String actualTrash = driver.findElement(By.id("l_trash")).getText();
 
-        System.out.println("The Calculator test is completed");
+        Assert.assertEquals(actualSLam,expectedSLam);
+        Assert.assertEquals(actualCount,expectedCount);
+        Assert.assertEquals(actualPacks,expectedPacks);
+        Assert.assertEquals(actualPrice,expectedPrice);
+        Assert.assertEquals(actualOver,expectedOver);
+        Assert.assertEquals(actualTrash,expectedTrash);
+
+        System.out.println("The lamCalculator test is completed");
     }
 
     @AfterTest(alwaysRun = true)
