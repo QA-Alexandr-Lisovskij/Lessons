97405b90651b8fb4748f1e8001fb4d9f7a9fc480
QA-Alexandr-Lisovskij <lisovskijsanya0@mail.ru>
2022-01-17 22:14:27 +0300

    Update

--- src/test/java/models/User.java
+++ src/test/java/models/User.java
@@ -1,28 +1,13 @@
 package models;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@Builder
+@ToString(exclude = "email")
 public class User {
     private String email;
     private String password;
-
-
-    public User() {
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public User setEmail(String email) {
-        this.email = email;
-        return this;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public User setPassword(String password) {
-        this.password = password;
-        return this;
-    }
 }
